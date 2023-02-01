4bc4cfa14789d7019536760b86a5c72d6d82a7ed
harshrai654 <harshraitth2@gmail.com>
2023-02-02 01:22:50 +0530

    IFogSim tutorial code v2

---
+++ src/org/fog/test/perfeval/MyFogDevice.java
@@ -0,0 +1,12 @@
+package org.fog.test.perfeval;
+
+public class MyFogDevice {
+    private int mips;
+
+    public int getMips() {
+        return mips;
+    }
+    public void setMips(int mips) {
+        this.mips = mips;
+    }
+}
